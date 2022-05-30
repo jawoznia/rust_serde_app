b7e25196f2ff83d1b92c6455c96c94ed1d283047
jawoznia <jnwznk@gmail.com>
2022-05-30 23:43:36 +0200

    Getting request back for some reason transforms id. I'm not sure why.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
 use serde::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Todo {
+    #[serde(rename = "userId")]
+    user_id: i32,
+    id: i32,
+    title: String,
+    completed: bool,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    let todos = reqwest::Client::new()
-        .get("https://jsonplaceholder.typicode.com/todos/1?userdId=1")
+    let todos: Vec<Todo> = reqwest::Client::new()
+        .get("https://jsonplaceholder.typicode.com/todos?userdId=1")
         .send()
         .await?
-        .text()
+        .json()
         .await?;
 
-    println!("{:?}", todos);
+    let sent_todo: Todo = reqwest::Client::new()
+        .post("https://jsonplaceholder.typicode.com/todos")
+        .json(&todos[4])
+        .send()
+        .await?
+        .json()
+        .await?;
+
+    println!("{:#?}", sent_todo);
+    println!("{:#?}", &todos[4]);
+
     Ok(())
 }
